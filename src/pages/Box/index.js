74e49807c6d717a14c054c68201b50ac29d51230
Luísa Barros <qlullyq@gmail.com>
2019-04-15 00:10:03 -0300

    Format data

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import { distanceInWords } from 'date-fns';
+import pt from 'date-fns/locale/pt'
+import Dropzone from 'react-dropzone'
 
 import { MdInsertDriveFile } from 'react-icons/md';
 
@@ -16,6 +19,17 @@ export default class Box extends Component {
     this.setState({ box: response.data });
   }
 
+  handleUpload = (files) => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+
+      data.append('file', file);
+
+      api.post(`boxes/${box}/files`, data);
+    });
+  }
+
   render() {
     return (
       <div id="box-container">
@@ -24,14 +38,28 @@ export default class Box extends Component {
           <h1>{this.state.box.title}</h1>
         </header>
 
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input { ...getInputProps() } />
+              <p>Arraste arquivos ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
+
         <ul>
           { this.state.box.files && this.state.box.files.map(file => (
-            <li>
+            <li key={file._id}>
               <a className="fileInfo" href={file.url} target="_blank">
                 <MdInsertDriveFile size={24} color="#A5CFFF" />
                 <strong>{file.title}</strong>
               </a>
-              <span>{file.createdAt}</span>
+              <span>
+                há{" "}
+                {distanceInWords(file.createdAt, new Date(), {
+                  locale: pt
+                })}
+              </span>
             </li>
           )) } 
         </ul>
